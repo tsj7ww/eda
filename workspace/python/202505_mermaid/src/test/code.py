27234eaf40be0aef79996d799d90e2285ca38bff
Trevor Jordan <20461601+tsj7ww@users.noreply.github.com>
2025-05-17 15:57:48 -0400

    work

---
+++ workspace/python/202505_mermaid/src/test/code.py
@@ -0,0 +1,28 @@
+class Stage1:
+    name = "stage1"
+    description = "abc"
+    inputs = ["in1_1", "in1_2", "in1_3"]
+    outputs = ["out1_1", "out1_2", "out1_3"]
+
+    def run(self):
+        print("Do something")
+
+
+class Stage2:
+    name = "stage1"
+    description = "abc"
+    inputs = ["in2_1", "in2_2", "out1_2"]
+    outputs = ["out2_1", "out2_2", "out2_3"]
+
+    def run(self):
+        print("Do something")
+
+
+class Stage3:
+    name = "stage1"
+    description = "abc"
+    inputs = ["out1_2", "out2_3", "out1_3", "out2_2"]
+    outputs = ["out3_1", "out3_2", "out3_3"]
+
+    def run(self):
+        print("Do something")
